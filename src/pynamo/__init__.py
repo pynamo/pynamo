8f26271530705b894f79b11bd3fbd20b00580969
Michael Kroher <michael@kroher.net>
2025-03-08 06:02:04 -0500

    update item

--- src/pynamo/__init__.py
+++ src/pynamo/__init__.py
@@ -8,12 +8,14 @@ from .attribute import Attribute
 from .model import Model
 from .get_item import GetItem
 from .put_item import PutItem
+from .update_item import UpdateItem
 
 __all__ = [
     "Table",
     "PrimaryIndex",
     "GetItem",
     "PutItem",
+    "UpdateItem",
     "GlobalSecondaryIndex",
     "LocalSecondaryIndex",
     "Attribute",
---
+++ tests/test_update_item.py
@@ -0,0 +1,33 @@
+from pynamo import Attribute, Model, UpdateItem, Table, PrimaryIndex
+from pynamo.fields import String
+
+
+def test_update_item():
+    mytable = Table(
+        "mytable",
+        PrimaryIndex(Attribute("PK", String)),
+    )
+
+    class Foo(Model):
+        __table__ = mytable
+
+        id = Attribute(
+            String,
+            primary_key=True,
+        )
+        name = Attribute(String)
+
+    foo = Foo(id="someid", name="SomeName")
+
+    foo.name = "ChangedName"  # type: ignore
+
+    update_request = UpdateItem(foo)
+
+    assert update_request.to_dynamodb() == {
+        "TableName": "mytable",
+        "Key": {"PK": {"S": "someid"}},
+        "UpdateExpression": "SET #ATTR0 = :ATTR0",
+        "ExpressionAttributeNames": {"#ATTR0": "name"},
+        "ExpressionAttributeValues": {":ATTR0": {"S": "ChangedName"}},
+        "ReturnValues": "ALL_NEW",
+    }
